c89e5739beda1e8e713742150f1e223185a70d75
williampetitpierre <williampp@hotmail.fr>
2024-09-02 16:57:42 +0200

    font and color variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById("summaryButton").addEventListener("click", function() {
-    document.getElementById("summarySection").classList.remove("hidden");
-    document.getElementById("summaryButton").classList.add("hidden");
+document.getElementById("summary-button").addEventListener("click", function() {
+    document.getElementById("summary-section").classList.remove("hidden");
+    document.getElementById("summary-button").classList.add("hidden");
 });
 
-document.getElementById("quizzButton").addEventListener("click", function() {
-    document.getElementById("quizzSection").classList.remove("hidden");
-    document.getElementById("quizzButton").classList.add("hidden");
+document.getElementById("quizz-button").addEventListener("click", function() {
+    document.getElementById("quizz-section").classList.remove("hidden");
+    document.getElementById("quizz-button").classList.add("hidden");
 });
 
-document.getElementById("quizzForm").addEventListener("submit", function(event) {
+document.getElementById("quizz-form").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("Quizz submitted! ");
 });
