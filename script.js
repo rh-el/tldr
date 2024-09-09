d995c8a2d03314243ba4c0ce335a3bd5ab46fc12
annekaftal <anne.kaftal@gmail.com>
2024-09-09 15:07:29 +0200

    blablabla

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const btn = document.getElementById('summary-button')
 
 try {
     btn.addEventListener('click', () => {
-        fetchAPI()
-
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: () => checkArticle(document)},
+                (results) => {
+                    results[0].result ? fetchAPI() : alert('No article found on this page...')
+                }
+            )
+        })
     })
 } catch (error) {
     console.log(error)
+}
+
+function checkArticle(document) {
+    return document.getElementsByTagName('article').length > 0 ? true : false
 }
 
 
