79a2c415b36c14b900753783400e28f0cf62e2cf
HelenemLH <helenemlagarde@gmail.com>
2024-09-09 16:41:02 +0200

    loader

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const btn = document.getElementById('summary-button')
+const btn = document.getElementById('summary-button');
+const dashboardBtn = document.getElementById('dashboard-button');
+const loader = document.querySelector('.loader');
 
 try {
     btn.addEventListener('click', () => {
@@ -18,23 +20,49 @@ function checkArticle(document) {
     return document.getElementsByTagName('article').length > 0 ? true : false
 }
 
+// show loader and hide buttons
+function showLoader() {
+    loader.classList.add('active');
+    btn.classList.add('hidden');          
+    dashboardBtn.classList.add('hidden'); 
+}
 
+// hide loader and show buttons
+function hideLoader() {
+    loader.classList.remove('active');
+    btn.classList.remove('hidden');          
+    dashboardBtn.classList.remove('hidden'); 
+}
+
+// EventListener for button click
+btn.addEventListener('click', () => {
+    showLoader(); 
+    fetchAPI(); 
+});
 
 function extractContents(document) {
-    const articleContent = document.querySelectorAll('article')[0].textContent
+    const articleContent = document.querySelectorAll('article')[0].textContent;
     return articleContent;
-};
-
-
+}
 
 async function fetchAPI() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: () => extractContents(document)},
-        async (results) => {                      
-            const aiResponse = await getAIResponse(results[0].result)
-            aiResponseJSON = JSON.parse(aiResponse);
-            createSummary(aiResponseJSON)
-        }
-      );
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId: tabs[0].id},
+                function: extractContents
+            },
+            async (results) => {  
+                try {
+                    const aiResponse = await getAIResponse(results[0].result);
+                    const aiResponseJSON = JSON.parse(aiResponse);
+                    createSummary(aiResponseJSON);
+                } catch (error) {
+                    console.error('Error fetching AI response:', error);
+                } finally {
+                    hideLoader(); 
+                }
+            }
+        );
     });
 }
