d7ce4f7ac6e0e1d1dd03810aafa47b9fcaabf456
williampetitpierre <williampp@hotmail.fr>
2024-09-09 10:48:15 +0200

    dashboard init

--- background.js
+++ background.js
@@ -17,8 +17,21 @@ chrome.contextMenus.onClicked.addListener((clickData) =>{
     if (clickData.menuItemId == "summarize" && clickData.selectionText) {
         const selectedText = clickData.selectionText
         summarizedText = selectedText
-        console.log(summarizeText)
-        // aiResponse = await getAIResponse(summarizedText)
-        // createSummary(responseTest)
+        summarizedFetchAPI(summarizedText)
     }
 })
+
+
+
+async function summarizedFetchAPI() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: () => extractContents(document)},
+        async (results) => {                 
+            const aiResponse = await getAIResponse(summarizedText)
+            aiResponseJSON = JSON.parse(aiResponse);
+            console.log(aiResponseJSON)
+            createSummary(aiResponseJSON)
+        }
+      );
+    });
+}
